475b902bf3e76e06429ebf7b66b3314f346dc47c
Sam Morris <sam@themarkup.org>
2020-01-28 13:54:38 -0500

    use rollup to compile the js

--- scripts/compile/javascript.js
+++ scripts/compile/javascript.js
@@ -1,7 +1,27 @@
 const fs = require('fs-extra');
+const rollup = require('rollup');
+const rollupResolve = require('rollup-plugin-node-resolve');
+const rollupCommonJs = require('rollup-plugin-commonjs');
+
+let inputOptions = {
+  plugins: [
+    rollupResolve(),
+    rollupCommonJs()
+  ]
+};
+
+const outputOptions = {
+  format: 'iife'
+};
 
  module.exports = {
-   render(path) {
-    // render the js
-   }
+    render(path) {
+      (async function() {
+        inputOptions.input = path;
+        const bundle = await rollup.rollup(inputOptions);
+        const output = await bundle.generate(outputOptions);
+        const fileName = path.replace(/^.*[\\\/]/, '');
+        fs.writeFileSync('.build/' + fileName, output.output[0].code);
+      })();
+    }
  }

6af7d2a0515c0c8ca3a33eb8d88eae2d7261e853
Sam Morris <sam@themarkup.org>
2020-05-11 18:19:25 -0400

    setup fallback images

--- scripts/compile/screenshot.js
+++ scripts/compile/screenshot.js
@@ -1,6 +1,7 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs-extra');
 const logger = require('../utilities/logger');
+const pathFinder = require('../utilities/pathfinder');
 
 module.exports = {
   take(graphic) {
@@ -8,14 +9,25 @@ module.exports = {
     logger.log('fallback', 'Taking screenshot...');
 
     (async () => {
+      // create browser
       let browser = await puppeteer.launch();
       let page = await browser.newPage();
-      const html = fs.readFileSync('.build/index.html', 'utf8');
+
+      // load preview html file
+      let html = fs.readFileSync('./.build/index.html', 'utf8');
+
+      // replace paths with local versions
+      const regEx = new RegExp(graphic.path, 'g');
+      html = html.replace(regEx, pathFinder.get('local', graphic));
+
+      // load html in browser
       await page.setContent(html);
 
+      // take screenshot
       let el = await page.$(`.graphics--${graphic.name} .graphics__content`);
       let image = await el.screenshot();
 
+      // save screenshot
       fs.writeFileSync('./.build/' + graphic.name + '/fallback.png', image);
 
       await page.close();

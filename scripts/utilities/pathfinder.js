c305505fc9915c882bed5ce4a2b8c859cc9ca522
Dan Phiffer <dan@phiffer.org>
2021-08-02 13:48:06 -0400

    Revert "update with self-hosted weepeople"
    This reverts commit bcd872676ad7f90d2a47b5c01cb9b918b47f7dde.

--- scripts/utilities/pathfinder.js
+++ scripts/utilities/pathfinder.js
@@ -1,7 +1,7 @@
 module.exports = {
   get(env, graphic) {
     if (env && env === 'remote') {
-      return `https://mrkp-static-staging.themarkup.org/graphics/${graphic.name}/${graphic.version}`;
+      return `https://mrkp-static-production.themarkup.org/graphics/${graphic.name}/${graphic.version}`;
     } else {
       return `http://localhost:5000/${graphic.name}`;
     }

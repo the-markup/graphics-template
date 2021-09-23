c0b0f325ab227f3f1b12da6f86c8ea76e94d8dc7
Dan Phiffer <dan@phiffer.org>
2021-09-23 16:15:45 -0400

    decide about env vars in node.js

--- scripts/remote.js
+++ scripts/remote.js
@@ -2,12 +2,12 @@ const cmd = require('node-cmd');
 
 module.exports = {
 	deploy(graphic) {
-		var env = '';
+		const profile = process.env.AWS_PROFILE || 'operations_prod';
+		var bucket = process.env.S3_UPLOADS_BUCKET || 'mrkp-wp-uploads-themarkup-org-production';
 		if (process.argv.indexOf('staging') > -1) {
-			env = 'S3_UPLOADS_BUCKET="mrkp-wp-uploads-themarkup-org-staging"'
+			bucket = 'mrkp-wp-uploads-themarkup-org-staging';
 		}
-		console.log(`${env} sh scripts/remote/deploy ${graphic.name} ${graphic.version}`);
-		process.exit();
+		const env = `AWS_PROFILE="${profile}" S3_UPLOADS_BUCKET="${bucket}"`;
 		cmd.get(`${env} sh scripts/remote/deploy ${graphic.name} ${graphic.version}`, (error, data) => {
 			if (error) {
 				console.log(error);

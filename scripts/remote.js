ab371ac6669d6afa973a663a1b17344c11f59995
Simon Fondrie-Teitler <simon.ft@themarkup.org>
2021-10-25 10:21:08 -0400

    auto-deploy

--- scripts/remote.js
+++ scripts/remote.js
@@ -4,11 +4,18 @@ module.exports = {
 	deploy(graphic) {
 		const profile = process.env.AWS_PROFILE || 'operations_prod';
 		var bucket = process.env.S3_UPLOADS_BUCKET || 'mrkp-wp-uploads-themarkup-org-production';
+		const repo_name = (process.env.GITHUB_REPOSITORY || "").replace("the-markup/graphics-", "");
 		if (process.argv.indexOf('staging') > -1) {
 			bucket = 'mrkp-wp-uploads-themarkup-org-staging';
 		}
-		const env = `AWS_PROFILE="${profile}" S3_UPLOADS_BUCKET="${bucket}"`;
-		cmd.get(`${env} sh scripts/remote/deploy ${graphic.name} ${graphic.version}`, (error, data) => {
+		var env = `S3_UPLOADS_BUCKET="${bucket}"`;
+
+		// if this is part of the github autodeploy don't set the profile
+		if (!repo_name) {
+			env = `${env} AWS_PROFILE="${profile}"`
+		}
+
+		cmd.get(`${env} bash scripts/remote/deploy ${graphic.name} ${graphic.version} ${repo_name} `, (error, data) => {
 			if (error) {
 				console.log(error);
 			}

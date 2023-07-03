ed4af41636d5cc69f90e587be4add1ec45599e87
Jerome Paulos <jeromepaulos@icloud.com>
2023-07-03 13:06:37 -0700

    Add TypeScript and Sass support

--- src/graphic/svelte/App.svelte
+++ src/graphic/svelte/App.svelte
@@ -1,5 +1,25 @@
-<script>
-	let message = 'This example comes from App.svelte.';
+<script lang="ts">
+	let message: string = 'This example comes from App.svelte!';
+	let typeScript: boolean = true;
+	let scss: boolean = true;
 </script>
 
-<p>{message}</p>
+<div class="messages">
+	<p>{message}</p>
+	{#if typeScript} <p class="typescript">With TypeScript!</p> {/if}
+	{#if scss} <p class="scss">With SCSS!</p> {/if}
+</div>
+
+<style lang="scss">
+	.messages {
+		.typescript {
+			color: white;
+			background-color: #3F74BA;
+		}
+
+		.scss {
+			color: white;
+			background-color: #BA6993;
+		}
+	}
+</style>

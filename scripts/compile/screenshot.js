498d963346eb2f2df52dcf240d16fbfc6d124e48
Miles Hilton <miles.p.hilton@gmail.com>
2025-02-20 11:26:39 -0500

    error handling for screenshot

--- scripts/compile/screenshot.js
+++ scripts/compile/screenshot.js
@@ -8,31 +8,36 @@ module.exports = {
 	async take(graphic) {
 		logger.log('fallback', 'Taking screenshot...');
 
-		let browser = await puppeteer.launch();
-		let page = await browser.newPage();
-		page.setViewport({
-			deviceScaleFactor: 2,
-			width: 1920,
-			height: 1080
-		});
+		try {
+			let browser = await puppeteer.launch();
+			let page = await browser.newPage();
+			page.setViewport({
+				deviceScaleFactor: 2,
+				width: 1920,
+				height: 1080
+			});
 
-		const indexPath = path.join('.', path.sep, '.build', 'index.html');
-		let html = fs.readFileSync(indexPath, 'utf8');
-		const regEx = new RegExp(graphic.path, 'g');
-		html = html.replace(regEx, pathFinder.get('local', graphic));
-		await page.setContent(html, {
-			waitUntil: 'load'
-		});
+			const indexPath = path.join('.', path.sep, '.build', 'index.html');
+			let html = fs.readFileSync(indexPath, 'utf8');
+			const regEx = new RegExp(graphic.path, 'g');
+			html = html.replace(regEx, pathFinder.get('local', graphic));
+			await page.setContent(html, {
+				waitUntil: 'load'
+			});
 
-		let el = await page.$(`.graphics--${graphic.name} .graphics__content`);
-		let image = await el.screenshot();
+			let el = await page.$(`.graphics--${graphic.name} .graphics__content`);
+			let image = await el.screenshot();
 
-		const imagePath = path.join('.', '.build', graphic.name, 'fallback.png');
-		fs.writeFileSync(imagePath, image);
+			const imagePath = path.join('.', '.build', graphic.name, 'fallback.png');
+			fs.writeFileSync(imagePath, image);
 
-		await page.close();
-		await browser.close();
+			await page.close();
+			await browser.close();
 
-		logger.log('fallback', 'Screenshot of graphic saved');
+			logger.log('fallback', 'Screenshot of graphic saved');
+		} catch(e) {
+			console.error("unable to take screenshot");
+			console.log(e);
+		}
 	}
 };
